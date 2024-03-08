4c3df36038bd45472d7f6dca90e8a010edd8efc6
kcollett <karen.collett@gmail.com>
2024-03-08 09:46:47 -0600

    Enabled whichwrap for 'l' and 'h'

--- lua/kcollett/core/options.lua
+++ lua/kcollett/core/options.lua
@@ -6,3 +6,7 @@ vim.opt.smarttab = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- See https://www.reddit.com/r/neovim/comments/yxy3te/cant_append_in_lua_options/
+vim.opt.whichwrap:append("l,h")
+-- vim syntax is set whichwrap+=h,l
